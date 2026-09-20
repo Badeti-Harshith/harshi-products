547d7ab49fed88930f2661ac09a569298a3066fe
Badeti-Harshith <harshithbaditeharshith@gmail.com>
2026-09-20 11:12:13 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,18 @@
+// Edit this file: add your electronics products here.
+window.PRODUCTS = [
+  {
+    name: "noise airwave xr",
+    description: "Short description of the product (replace this).",
+    // Optional image: you can use any image URL you own/have permission to use
+    image: "https://www.flipkart.com/full-image-view",
+    amazonLink: "PASTE_AMAZON_LINK_HERE",
+    flipkartLink: "https://dl.flipkart.com/s/8BQH0JuuuN"
+  },
+  {
+    name: "Sample Electronics 2",
+    description: "Short description of the second product (replace this).",
+    image: "",
+    amazonLink: "PASTE_AMAZON_LINK_HERE",
+    flipkartLink: "PASTE_FLIPKART_LINK_HERE"
+  }
+];
