e499ff39436e20d69229a7e20bbb86ed9ef2ad94
Badeti-Harshith <harshithbaditeharshith@gmail.com>
2026-09-26 12:05:19 +0530

    Change image URL for noise airwave xr product
    Updated the image URL for the 'noise airwave xr' product.

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ window.PRODUCTS = [
     name: "noise airwave xr",
     description: "Short description of the product (replace this).",
     // Optional image: you can use any image URL you own/have permission to use
-    image: "https://m.media-amazon.com/images/I/61ttOStDEqL._SL1500_.jpg",
+    image: "https://rukminim1.flixcart.com/image/982/982/xif0q/headphone/j/n/r/-enriched-transparent-original-imahhtr3jggp5fgh.png?q=90",
     amazonLink: "PASTE_AMAZON_LINK_HERE",
     flipkartLink: "https://dl.flipkart.com/s/8BQH0JuuuN"
   },
